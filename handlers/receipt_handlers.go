c277229c986dd4ad578149657ca86ac360060ac2
Jakob Lybarger <jakoblybarger911@gmail.com>
2025-01-30 22:05:55 -0600

    Remove print statements

--- handlers/receipt_handlers.go
+++ handlers/receipt_handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"math"
 	"net/http"
 	"strconv"
@@ -68,59 +67,37 @@ func calculateReceiptPoints(receipt models.Receipt) int {
 		}
 	}
 
-	fmt.Printf("Points after retiler name %d\n", points)
-
-	prev := points
-
 	total, _ := strconv.ParseFloat(receipt.Total, 64)
 	if total == math.Trunc(total) {
 		points += 50
 	}
 
-	fmt.Printf("Points after seeing if total is round dollar %d - %d \n", points, points-prev)
-	prev = points
-
 	if math.Mod(total, 0.25) == 0 {
 		points += 25
 	}
-	fmt.Printf("Points after seeing if is multiple of 0.25 %d - %d\n", points, points-prev)
-	prev = points
 
 	for i := 1; i < len(receipt.Items); i += 2 {
 		points += 5
 	}
-	fmt.Printf("Points after for every 2 items %d - %d\n", points, points-prev)
-	prev = points
 
 	for _, item := range receipt.Items {
 		strLen := float64(len(strings.TrimSpace(item.ShortDescription)))
 		if math.Mod(strLen, 3) == 0 {
 
-			fmt.Printf("'%s'\n'%s'\n", item.ShortDescription, strings.TrimSpace(item.ShortDescription))
 			price, _ := strconv.ParseFloat(item.Price, 64)
-			fmt.Printf("price %f\n", price)
-			newpoints := int(math.Ceil(price * 0.2))
-			fmt.Printf("new points %d\n", newpoints)
 			points += int(math.Ceil(price * 0.2))
 		}
 	}
-	fmt.Printf("Points after trimmed description %d - %d\n", points, points-prev)
-	prev = points
 
 	d, _ := time.Parse("2006-01-02", receipt.PurchaseDate)
 	if math.Mod(float64(d.Day()), 2) != 0 {
-		fmt.Printf("Days is %d", d.YearDay())
 		points += 6
 	}
-	fmt.Printf("Points after checking if purchase date is odd %d - %d\n", points, points-prev)
-	prev = points
 
 	d, _ = time.Parse("15:04", receipt.PurchaseTime)
-	fmt.Printf("Hour is %d\n", d.Hour())
 	if 14 <= d.Hour() && d.Hour() < 16 {
 		points += 10
 	}
-	fmt.Printf("Points after looking at purchase time %d - %d\n", points, points-prev)
 
 	return points
 }
